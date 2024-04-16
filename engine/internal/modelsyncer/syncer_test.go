205a721e7dc1d7471bbc1e00ead71e84f67c3ea8
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-16 09:42:53 -0700

    Remove the model name conversion for Ollama
    We have changed the original model ID format.

--- engine/internal/modelsyncer/syncer_test.go
+++ engine/internal/modelsyncer/syncer_test.go
@@ -41,13 +41,22 @@ func TestOllamaModelName(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			modelID: "ft:gemma:2b:custom-model-name:7p4lURel",
+			modelID: "ft:gemma:2b-custom-model-name-7p4lURel",
 			want:    "gemma:2b-custom-model-name-7p4lURel",
+		},
+		{
+			modelID: "bogus",
+			wantErr: true,
 		},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.modelID, func(t *testing.T) {
-			got := ollamaModelName(tc.modelID)
+			got, err := ollamaModelName(tc.modelID)
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
 			assert.Equal(t, tc.want, got)
 		})
 	}

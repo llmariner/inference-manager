63d8ac10581dc62af3d676369108ec08661261f1
Jun Mukai <jun.mukai@gmail.com>
2024-04-19 16:34:43 -0700

    Add CI/CD for inference manager. (#20)

--- engine/internal/ollama/manager.go
+++ engine/internal/ollama/manager.go
@@ -22,7 +22,9 @@ type Manager struct {
 func (m *Manager) Run() error {
 	log.Printf("Starting Ollama on port %d\n", m.port)
 
-	os.Setenv("OLLAMA_HOST", fmt.Sprintf("0.0.0.0:%d", m.port))
+	if err := os.Setenv("OLLAMA_HOST", fmt.Sprintf("0.0.0.0:%d", m.port)); err != nil {
+		return err
+	}
 	_, err := exec.Command("ollama", "serve").Output()
 	return err
 }
@@ -39,7 +41,11 @@ func (m *Manager) CreateNewModel(modelName string, spec *ModelSpec) error {
 	if err != nil {
 		return err
 	}
-	defer os.Remove(file.Name())
+	defer func() {
+		if err := os.Remove(file.Name()); err != nil {
+			log.Printf("Failed to remove %q: %s", file.Name(), err)
+		}
+	}()
 
 	s := fmt.Sprintf("FROM %s\nAdapter %s\n", spec.BaseModel, spec.AdapterPath)
 	if _, err := file.Write([]byte(s)); err != nil {
@@ -49,7 +55,9 @@ func (m *Manager) CreateNewModel(modelName string, spec *ModelSpec) error {
 		return err
 	}
 
-	os.Setenv("OLLAMA_HOST", fmt.Sprintf("0.0.0.0:%d", m.port))
+	if err := os.Setenv("OLLAMA_HOST", fmt.Sprintf("0.0.0.0:%d", m.port)); err != nil {
+		return err
+	}
 	cmd := exec.Command("ollama", "create", modelName, "-f", file.Name())
 	var errb bytes.Buffer
 	cmd.Stderr = &errb

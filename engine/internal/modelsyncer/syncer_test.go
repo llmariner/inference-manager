d3febf89f100328e7a3cc4d88ffcb1ede97c3201
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-26 12:08:56 -0700

    Sync base models from model manager (#36)

--- engine/internal/modelsyncer/syncer_test.go
+++ engine/internal/modelsyncer/syncer_test.go
@@ -1,10 +1,71 @@
 package modelsyncer
 
 import (
+	"context"
+	"io"
 	"testing"
 
+	"github.com/llm-operator/inference-manager/engine/internal/ollama"
+	mv1 "github.com/llm-operator/model-manager/api/v1"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
+
+func TestSyncModels(t *testing.T) {
+	tcs := []struct {
+		name                 string
+		models               []*mv1.Model
+		wantRegisteredModels []string
+	}{
+		{
+			name:                 "no models",
+			models:               []*mv1.Model{},
+			wantRegisteredModels: nil,
+		},
+		{
+			name: "system model",
+			models: []*mv1.Model{
+				{
+					Id:      "google/gemma-2b",
+					OwnedBy: systemOwner,
+				},
+			},
+			wantRegisteredModels: []string{
+				"google/gemma-2b",
+			},
+		},
+		{
+			name: "non-system model",
+			models: []*mv1.Model{
+				{
+					Id:      "ft:google-gemma-2b:fine-tuning-wpsd9kb5nl",
+					OwnedBy: fakeTenantID,
+				},
+			},
+			wantRegisteredModels: []string{
+				"ft:google-gemma-2b:fine-tuning-wpsd9kb5nl",
+			},
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			om := New(
+				&noopOllamaManager{},
+				&noopS3Client{},
+				&fakeModelClient{models: tc.models},
+				&fakeModelInternalClient{},
+			)
+			err := om.syncModels(context.Background())
+			assert.NoError(t, err)
+
+			var registered []string
+			for k := range om.registeredModels {
+				registered = append(registered, k)
+			}
+			assert.ElementsMatch(t, tc.wantRegisteredModels, registered)
+		})
+	}
+}
 
 func TestExtractBaseModel(t *testing.T) {
 	tcs := []struct {
@@ -60,4 +121,41 @@ func TestOllamaModelName(t *testing.T) {
 			assert.Equal(t, tc.want, got)
 		})
 	}
+}
+
+type noopOllamaManager struct {
+}
+
+func (n *noopOllamaManager) CreateNewModel(modelName string, spec *ollama.ModelSpec) error {
+	return nil
+}
+
+func (n *noopOllamaManager) PullBaseModel(modelName string) error {
+	return nil
+}
+
+type noopS3Client struct {
+}
+
+func (n *noopS3Client) Download(f io.WriterAt, path string) error {
+	return nil
+}
+
+type fakeModelClient struct {
+	models []*mv1.Model
+}
+
+func (n *fakeModelClient) ListModels(ctx context.Context, in *mv1.ListModelsRequest, opts ...grpc.CallOption) (*mv1.ListModelsResponse, error) {
+	return &mv1.ListModelsResponse{
+		Data: n.models,
+	}, nil
+}
+
+type fakeModelInternalClient struct {
+}
+
+func (n *fakeModelInternalClient) GetModelPath(ctx context.Context, in *mv1.GetModelPathRequest, opts ...grpc.CallOption) (*mv1.GetModelPathResponse, error) {
+	return &mv1.GetModelPathResponse{
+		Path: "fake-path",
+	}, nil
 }

94e37ce28ca3b35c44c5c51a1a64d4e4c9e1f721
Aya Igarashi <ladiclexxx@gmail.com>
2024-05-01 10:10:35 +0900

    Authorize token by rbac-server

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
 	v1 "github.com/llm-operator/inference-manager/api/v1"
+	"github.com/llm-operator/inference-manager/server/internal/config"
+	"github.com/llm-operator/rbac-manager/pkg/auth"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -23,10 +26,19 @@ type S struct {
 }
 
 // Run starts the gRPC server.
-func (s *S) Run(port int) error {
+func (s *S) Run(ctx context.Context, port int, authConfig config.AuthConfig) error {
 	log.Printf("Starting server on port %d\n", port)
 
-	grpcServer := grpc.NewServer()
+	var opts []grpc.ServerOption
+	if authConfig.Enable {
+		ai, err := auth.NewInterceptor(ctx, authConfig.RBACInternalServerAddr, "api.model")
+		if err != nil {
+			return err
+		}
+		opts = append(opts, grpc.ChainUnaryInterceptor(ai.Unary()))
+	}
+
+	grpcServer := grpc.NewServer(opts...)
 	v1.RegisterChatServiceServer(grpcServer, s)
 	reflection.Register(grpcServer)
 

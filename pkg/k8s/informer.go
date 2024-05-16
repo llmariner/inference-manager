1a6b110b8a09a81f435806bcb95871f7aed08efb
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-16 16:02:15 -0700

    Merge functions in pkg/k8s/informer.go (#77)
    Some helper functions are not giving much value, and
    just merging these functions would improve readability.

--- pkg/k8s/informer.go
+++ pkg/k8s/informer.go
@@ -29,8 +29,6 @@ func NewInformers(
 	namespace string,
 	labels map[string]string,
 ) (*Informers, error) {
-	is := &Informers{stopCh: stopCh}
-
 	selector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
 		MatchLabels: labels,
 	})
@@ -46,9 +44,10 @@ func NewInformers(
 		}),
 	)
 
-	is.podInformer = createPodInformer(factory)
-
-	return is, nil
+	return &Informers{
+		stopCh:      stopCh,
+		podInformer: factory.Core().V1().Pods().Informer(),
+	}, nil
 }
 
 // Start starts the informers.
@@ -57,26 +56,12 @@ func (is *Informers) Start() {
 	go is.podInformer.Run(is.stopCh)
 }
 
-// createPodInformer creates an informer for Pod resources.
-func createPodInformer(f informers.SharedInformerFactory) cache.SharedIndexInformer {
-	return f.Core().V1().Pods().Informer()
-}
-
 // SetPodEventHandlers sets the specified handlers to the Pod informer.
 func (is *Informers) SetPodEventHandlers(
 	ctx context.Context,
 	handlers []EventHandlerWithContext,
 ) error {
-	return is.setEventHandlers(ctx, is.podInformer, handlers)
-}
-
-// setEventHandlers set event handlers to the informer.
-func (is *Informers) setEventHandlers(
-	ctx context.Context,
-	infrmr cache.SharedIndexInformer,
-	handlers []EventHandlerWithContext,
-) error {
-	_, err := infrmr.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	_, err := is.podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			for _, handler := range handlers {
 				handler.ProcessAdd(ctx, obj)

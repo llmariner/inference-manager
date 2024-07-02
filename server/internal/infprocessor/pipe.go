46c237b01de5ae6368a417eb61ade0ecb4c17e0e
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-07-01 17:48:12 -0700

    Define pipeReadWriteCloser (#130)
    This is used to write a server-sent-event that is
    sent from an engine to a task.RespCh.

---
+++ server/internal/infprocessor/pipe.go
@@ -0,0 +1,27 @@
+package infprocessor
+
+import "io"
+
+func newPipeReadWriteCloser() pipeReadWriteCloser {
+	pr, pw := io.Pipe()
+	return pipeReadWriteCloser{
+		PipeReader: pr,
+		PipeWriter: pw,
+	}
+}
+
+type pipeReadWriteCloser struct {
+	*io.PipeReader
+	*io.PipeWriter
+}
+
+// Close closes the pipe.
+func (c pipeReadWriteCloser) Close() error {
+	if err := c.PipeReader.Close(); err != nil {
+		return err
+	}
+	if err := c.PipeWriter.Close(); err != nil {
+		return err
+	}
+	return nil
+}

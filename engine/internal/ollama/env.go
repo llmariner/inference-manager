b17b00f536b754b455b47c98db67e049084adfeb
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-08-15 14:08:30 -0700

    Be able to configure Ollama numParallel
    This controls the maxiumum number of requests.

---
+++ engine/internal/ollama/env.go
@@ -0,0 +1,35 @@
+package ollama
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/llm-operator/inference-manager/engine/internal/config"
+)
+
+// SetEnvVarsFromConfig sets environment variables from the given configuration.
+func SetEnvVarsFromConfig(c *config.OllamaConfig) error {
+	if c.NumParallel > 0 {
+		log.Printf("Setting Ollama NumParallel %d\n", c.NumParallel)
+		if err := os.Setenv("OLLAMA_NUM_PARALLEL", strconv.Itoa(c.NumParallel)); err != nil {
+			return err
+		}
+	}
+
+	if c.ForceSpreading {
+		log.Printf("Enabling Ollama Force spreading.\n")
+		if err := os.Setenv("OLLAMA_SCHED_SPREAD", "true"); err != nil {
+			return err
+		}
+	}
+
+	if c.Debug {
+		log.Printf("Enabling Ollama debug mode.\n")
+		if err := os.Setenv("OLLAMA_DEBUG", "true"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

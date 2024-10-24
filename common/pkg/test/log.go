1ed1e4ddaa201c3c2ae86324060729f6211f3916
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-10-24 16:50:36 -0700

    fix

--- common/pkg/test/log.go
+++ common/pkg/test/log.go
@@ -2,19 +2,17 @@ package test
 
 import (
 	"context"
-	"log"
 	"strings"
 	"testing"
 
 	"github.com/go-logr/logr"
-	"github.com/go-logr/stdr"
+	"github.com/go-logr/logr/testr"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewTestLogger returns a logger that writes to the test log.
 func NewTestLogger(t *testing.T) logr.Logger {
-	logger := log.New(&testLogWriter{t}, "TEST: ", 0)
-	return stdr.New(logger)
+	return testr.NewWithOptions(t, testr.Options{Verbosity: 8})
 }
 
 // ContextWithLogger returns a context with a logger that writes to the test log.
@@ -30,9 +28,4 @@ type testLogWriter struct {
 func (w *testLogWriter) Write(p []byte) (n int, err error) {
 	w.t.Log(strings.TrimSpace(string(p)))
 	return len(p), nil
-}
-
-func init() {
-	// For dubugging. We set this from here to avoid race conditions.
-	stdr.SetVerbosity(8)
 }

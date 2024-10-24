516fde16168e322954f5fc57159284274aa665d0
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-10-24 16:53:19 -0700

    fix(common): fix a race condition in NewTestLogger (#447)
    To fix a test failure like below:
    
    https://github.com/llmariner/inference-manager/actions/runs/11504696862/job/32024755965

--- common/pkg/test/log.go
+++ common/pkg/test/log.go
@@ -2,20 +2,17 @@ package test
 
 import (
 	"context"
-	"log"
 	"strings"
 	"testing"
 
 	"github.com/go-logr/logr"
-	"github.com/go-logr/stdr"
+	"github.com/go-logr/logr/testr"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewTestLogger returns a logger that writes to the test log.
 func NewTestLogger(t *testing.T) logr.Logger {
-	stdr.SetVerbosity(8)
-	logger := log.New(&testLogWriter{t}, "TEST: ", 0)
-	return stdr.New(logger)
+	return testr.NewWithOptions(t, testr.Options{Verbosity: 8})
 }
 
 // ContextWithLogger returns a context with a logger that writes to the test log.

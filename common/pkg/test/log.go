ea65a7d9a79049f8e0a55791dc04accbc90e18c7
Aya Igarashi <ladiclexxx@gmail.com>
2024-11-08 09:27:20 +0900

    refactor(log): remove unused struct

--- common/pkg/test/log.go
+++ common/pkg/test/log.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"context"
-	"strings"
 	"testing"
 
 	"github.com/go-logr/logr"
@@ -19,13 +18,4 @@ func NewTestLogger(t *testing.T) logr.Logger {
 func ContextWithLogger(t *testing.T) context.Context {
 	logger := NewTestLogger(t)
 	return ctrl.LoggerInto(context.Background(), logger)
-}
-
-type testLogWriter struct {
-	t *testing.T
-}
-
-func (w *testLogWriter) Write(p []byte) (n int, err error) {
-	w.t.Log(strings.TrimSpace(string(p)))
-	return len(p), nil
 }

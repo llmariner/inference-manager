4277086e572046a4f5b3ec3412c1dc11b55a4162
Kenji Kaneda <kenji@Kenjis-MacBook-Air.local>
2025-02-21 17:20:28 -0800

    chore: add a test to GetGPU

--- engine/internal/runtime/manager_test.go
+++ engine/internal/runtime/manager_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apiruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -569,6 +570,92 @@ func TestAllChildrenUnschedulable(t *testing.T) {
 			unschedulable, err := allChildrenUnschedulable(ctx, k8sClient, sts)
 			assert.NoError(t, err)
 			assert.Equal(t, tc.want, unschedulable)
+		})
+	}
+}
+
+func TestGetGPU(t *testing.T) {
+	tcs := []struct {
+		name string
+		sts  appsv1.StatefulSet
+		want int32
+	}{
+		{
+			name: "gpu",
+			sts: appsv1.StatefulSet{
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Resources: corev1.ResourceRequirements{
+										Limits: corev1.ResourceList{
+											nvidiaGPUResource: resource.MustParse("1"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "no gpu",
+			sts: appsv1.StatefulSet{
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Resources: corev1.ResourceRequirements{
+										Limits: corev1.ResourceList{
+											"cpu": resource.MustParse("1"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "multiple containers",
+			sts: appsv1.StatefulSet{
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Resources: corev1.ResourceRequirements{
+										Limits: corev1.ResourceList{
+											nvidiaGPUResource: resource.MustParse("1"),
+										},
+									},
+								},
+								{
+									Resources: corev1.ResourceRequirements{
+										Limits: corev1.ResourceList{
+											nvidiaGPUResource: resource.MustParse("2"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: 3,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			got := getGPU(tc.sts)
+			assert.Equal(t, tc.want, got)
 		})
 	}
 }

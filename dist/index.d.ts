dbe5e5f523c5116c9eb690a2c7b560a247ef2194
Takeshi Yoshino <81723316+takeshi-cloudnatix@users.noreply.github.com>
2025-03-11 07:48:10 +0000

    feat(api): generate `index.js` and `index.d.ts` in the `dist` directory

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./api/v1/inference_server.pb";
+export * from "./api/v1/inference_server_embeddings.pb";
+export * from "./api/v1/inference_server_status.pb";
+export * from "./api/v1/inference_server_worker.pb";
+export * from "./fetch.pb";

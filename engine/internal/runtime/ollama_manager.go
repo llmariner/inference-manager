42cef1f9a4085c8a1d029b43812d2282ba659365
Aya Igarashi <Ladiclexxx@gmail.com>
2025-03-25 08:56:11 +0900

    fix(ollama/dynamic): gracefully handle HTTP requests and redeploy if the runtime is deleted (#616)

--- engine/internal/runtime/ollama_manager.go
+++ engine/internal/runtime/ollama_manager.go
@@ -87,6 +87,16 @@ func (m *OllamaManager) listModels(ready bool) []ModelRuntimeInfo {
 	return ms
 }
 
+func (m *OllamaManager) cleanupModels() {
+	for _, model := range m.models {
+		if model.ready {
+			continue
+		}
+		close(model.waitCh)
+	}
+	m.models = make(map[string]ollamaModel)
+}
+
 // Start deploys the ollama runtime.
 func (m *OllamaManager) Start(ctx context.Context) error {
 	sts, err := m.ollamaClient.DeployRuntime(ctx, "", true)
@@ -125,10 +135,19 @@ func (m *OllamaManager) PullModel(ctx context.Context, modelID string) error {
 		m.mu.RUnlock()
 		select {
 		case <-ch:
-		// TODO(aya): check error reason
 		case <-ctx.Done():
 			return ctx.Err()
 		}
+
+		m.mu.Lock()
+		if !m.runtime.ready {
+			err := fmt.Errorf("runtime is not ready: %s", m.runtime.errReason)
+			log.Error(err, "Runtime is not ready", "address", runtimeAddr)
+			m.mu.Unlock()
+			return err
+		}
+		runtimeAddr = m.runtime.address
+		m.mu.Unlock()
 		log.Info("Runtime is ready", "address", runtimeAddr)
 	}
 
@@ -144,46 +163,57 @@ func (m *OllamaManager) PullModel(ctx context.Context, modelID string) error {
 			case <-ctx.Done():
 				return ctx.Err()
 			}
+
+			m.mu.Lock()
+			if m.models[modelID].ready {
+				m.mu.Unlock()
+				log.Info("Model is pulled", "modelID", modelID)
+				return nil
+			}
+			m.models[modelID] = ollamaModel{id: modelID, waitCh: make(chan struct{})}
+			m.mu.Unlock()
+			log.Info("Model pulling is canceled, retrying", "modelID", modelID)
 		}
-		log.Info("Model is pulled", "modelID", modelID)
-		return nil
+	} else {
+		m.models[modelID] = ollamaModel{id: modelID, waitCh: make(chan struct{})}
+		m.mu.Unlock()
 	}
-	m.models[modelID] = ollamaModel{id: modelID, waitCh: make(chan struct{})}
-	m.mu.Unlock()
 	log.Info("Model is being pulled", "modelID", modelID)
 
 	// request to pull the model.
-	pullURL := &url.URL{Scheme: "http", Host: m.pullerAddr, Path: "/pull"}
-	resp, err := http.Post(pullURL.String(), "application/json",
-		bytes.NewBuffer(fmt.Appendf([]byte{}, `{"modelID": "%s"}`, modelID)))
-	if err != nil {
+	const requestTimeout = 3 * time.Second
+	const retryInterval = 2 * time.Second
+	pullURL := url.URL{Scheme: "http", Host: m.pullerAddr, Path: "/pull"}
+	pullData := fmt.Appendf([]byte{}, `{"modelID": "%s"}`, modelID)
+	if err := sendHTTPRequestWithRetry(ctx, pullURL, pullData, func(status int, err error) (bool, error) {
+		if err != nil {
+			log.V(2).Error(err, "Failed to pull model", "url", pullURL, "retry-interval", retryInterval)
+			return true, nil
+		}
+		if status != http.StatusAccepted {
+			return false, fmt.Errorf("unexpected status code: %d", status)
+		}
+		return false, nil
+	}, requestTimeout, retryInterval, 3); err != nil {
 		return fmt.Errorf("failed to pull model: %s", err)
-	}
-	if err := resp.Body.Close(); err != nil {
-		return fmt.Errorf("failed to close response body: %s", err)
-	}
-	if resp.StatusCode != http.StatusAccepted {
-		return fmt.Errorf("unexpected status: %s", resp.Status)
 	}
 
 	// wait until the model is ready.
 	log.Info("Waiting for the model to be ready", "modelID", modelID)
-	const retry = 2 * time.Second
-	showURL := &url.URL{Scheme: "http", Host: runtimeAddr, Path: "/api/show"}
-	for {
-		resp, err := http.Post(showURL.String(), "application/json",
-			bytes.NewBuffer(fmt.Appendf([]byte{}, `{"model": "%s"}`, modelID)))
+	showURL := url.URL{Scheme: "http", Host: runtimeAddr, Path: "/api/show"}
+	data := fmt.Appendf([]byte{}, `{"model": "%s"}`, modelID)
+	if err := sendHTTPRequestWithRetry(ctx, showURL, data, func(status int, err error) (bool, error) {
 		if err != nil {
-			return fmt.Errorf("failed to check model: %s", err)
+			log.V(2).Error(err, "Failed to check model status", "url", showURL, "retry-interval", retryInterval)
+			return true, nil
 		}
-		if err := resp.Body.Close(); err != nil {
-			return fmt.Errorf("failed to close response body: %s", err)
+		if status != http.StatusOK {
+			log.V(2).Info("Model is not ready yet", "status", status, "retry-interval", retryInterval)
+			return true, nil
 		}
-		if resp.StatusCode == http.StatusOK {
-			break
-		}
-		log.V(2).Info("Model is not ready yet", "url", showURL, "retry-after", retry, "status", resp.Status)
-		time.Sleep(retry)
+		return false, nil
+	}, requestTimeout, retryInterval, -1); err != nil {
+		return fmt.Errorf("failed to check model: %s", err)
 	}
 
 	log.Info("Model is ready", "modelID", modelID)
@@ -198,6 +228,39 @@ func (m *OllamaManager) PullModel(ctx context.Context, modelID string) error {
 	return nil
 }
 
+func sendHTTPRequestWithRetry(
+	ctx context.Context,
+	url url.URL, data []byte, retry func(status int, err error) (bool, error),
+	reqTimeout, retryInterval time.Duration, retryCount int,
+) error {
+	for attempt := 1; retryCount < 0 || attempt <= retryCount; attempt++ {
+		reqCtx, cancel := context.WithTimeout(ctx, reqTimeout)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(reqCtx, "POST", url.String(), bytes.NewBuffer(data))
+		if err != nil {
+			return fmt.Errorf("request creation error: %s", err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		var status int
+		if err == nil {
+			if err := resp.Body.Close(); err != nil {
+				return fmt.Errorf("failed to close response body: %s", err)
+			}
+			status = resp.StatusCode
+		}
+		if ok, err := retry(status, err); err != nil {
+			return err
+		} else if !ok {
+			return nil
+		}
+		time.Sleep(retryInterval)
+	}
+	return fmt.Errorf("retry count exceeded")
+}
+
 // Reconcile reconciles the runtime.
 func (m *OllamaManager) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := ctrl.LoggerFrom(ctx)
@@ -206,10 +269,18 @@ func (m *OllamaManager) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	if err := m.k8sClient.Get(ctx, req.NamespacedName, &sts); err != nil {
 		if apierrors.IsNotFound(err) {
 			m.mu.Lock()
-			m.runtime = runtime{}
+			m.runtime = newPendingRuntime(sts.Name)
+			m.cleanupModels()
 			m.mu.Unlock()
 			m.autoscaler.Unregister(req.NamespacedName)
 			log.Info("Runtime is deleted")
+
+			// Re-deploy the runtime.
+			sts, err := m.ollamaClient.DeployRuntime(ctx, "", false)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("deploy runtime: %s", err)
+			}
+			return ctrl.Result{}, m.autoscaler.Register(ctx, "", sts)
 		}
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
@@ -224,6 +295,7 @@ func (m *OllamaManager) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		if sts.Status.Replicas == 0 {
 			m.mu.Lock()
 			m.runtime = newPendingRuntime(sts.Name)
+			m.cleanupModels()
 			m.mu.Unlock()
 			log.Info("Runtime is scale down to zero")
 		} else {

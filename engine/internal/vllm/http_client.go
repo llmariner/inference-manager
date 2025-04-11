a99b5a6dd92e690dee4a86e687c47bb98b1c9c62
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-04-11 15:07:48 -0700

    feat: add an HTTP client for LoRA loading (#646)

---
+++ engine/internal/vllm/http_client.go
@@ -0,0 +1,62 @@
+package vllm
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// NewHTTPClient creates a new HTTP client with the given address.
+func NewHTTPClient(addr string) *HTTPClient {
+	return &HTTPClient{
+		addr: addr,
+	}
+}
+
+// HTTPClient is a struct that represents an HTTP client.
+type HTTPClient struct {
+	addr string
+}
+
+// LoadLoRAAdapter loads a LoRA adapter from the given path.
+func (c *HTTPClient) LoadLoRAAdapter(ctx context.Context, loraName, loraPath string) error {
+	type request struct {
+		LoRAName string `json:"lora_name"`
+		LoRAPath string `json:"lora_path"`
+	}
+
+	req := request{
+		LoRAName: loraName,
+		LoRAPath: loraPath,
+	}
+	data, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("marshal request: %s", err)
+	}
+
+	url := url.URL{Scheme: "http", Host: c.addr, Path: "/v1/load_lora_adapter"}
+	hreq, err := http.NewRequestWithContext(ctx, "POST", url.String(), bytes.NewBuffer(data))
+	if err != nil {
+		return fmt.Errorf("request creation error: %s", err)
+	}
+	hreq.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(hreq)
+	if err != nil {
+		return fmt.Errorf("send request: %s", err)
+	}
+
+	if err := resp.Body.Close(); err != nil {
+		return fmt.Errorf("close response body: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("load lora adapter: %s", resp.Status)
+	}
+
+	return nil
+}

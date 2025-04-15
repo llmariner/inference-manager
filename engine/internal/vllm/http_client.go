ce72c842e618bf10fcfdb846004fa8b1be6fe07e
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-04-15 15:38:01 -0700

    feat: add vllm ulload LoRA client

--- engine/internal/vllm/http_client.go
+++ engine/internal/vllm/http_client.go
@@ -37,15 +37,7 @@ func (c *HTTPClient) LoadLoRAAdapter(ctx context.Context, loraName, loraPath str
 		return -1, fmt.Errorf("marshal request: %s", err)
 	}
 
-	url := url.URL{Scheme: "http", Host: c.addr, Path: "/v1/load_lora_adapter"}
-	hreq, err := http.NewRequestWithContext(ctx, "POST", url.String(), bytes.NewBuffer(data))
-	if err != nil {
-		return -1, fmt.Errorf("request creation error: %s", err)
-	}
-	hreq.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(hreq)
+	resp, err := c.sendHTTPRequest(ctx, "POST", "/v1/load_lora_adapter", data)
 	if err != nil {
 		return -1, fmt.Errorf("send request: %s", err)
 	}
@@ -55,4 +47,52 @@ func (c *HTTPClient) LoadLoRAAdapter(ctx context.Context, loraName, loraPath str
 	}
 
 	return resp.StatusCode, nil
+}
+
+// UnloadLoRAAdapter unnloads a LoRA adapter from the given path.
+func (c *HTTPClient) UnloadLoRAAdapter(ctx context.Context, loraName string) (int, error) {
+	type request struct {
+		LoRAName string `json:"lora_name"`
+	}
+
+	req := request{
+		LoRAName: loraName,
+	}
+	data, err := json.Marshal(req)
+	if err != nil {
+		return -1, fmt.Errorf("marshal request: %s", err)
+	}
+
+	resp, err := c.sendHTTPRequest(ctx, "POST", "/v1/unload_lora_adapter", data)
+	if err != nil {
+		return -1, fmt.Errorf("send request: %s", err)
+	}
+
+	if err := resp.Body.Close(); err != nil {
+		return -1, fmt.Errorf("close response body: %s", err)
+	}
+
+	return resp.StatusCode, nil
+}
+
+func (c *HTTPClient) sendHTTPRequest(
+	ctx context.Context,
+	method string,
+	path string,
+	body []byte,
+) (*http.Response, error) {
+	url := url.URL{Scheme: "http", Host: c.addr, Path: path}
+	hreq, err := http.NewRequestWithContext(ctx, method, url.String(), bytes.NewBuffer(body))
+	if err != nil {
+		return nil, fmt.Errorf("request creation error: %s", err)
+	}
+	hreq.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(hreq)
+	if err != nil {
+		return nil, fmt.Errorf("send request: %s", err)
+	}
+
+	return resp, nil
 }

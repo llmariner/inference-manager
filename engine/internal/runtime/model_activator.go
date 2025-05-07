a005409cc413a2de8f0dcb8fe086e16b65d2efc5
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-05-07 16:24:37 -0700

    feat: manage model activation status from model-manager (#753)

---
+++ engine/internal/runtime/model_activator.go
@@ -0,0 +1,99 @@
+package runtime
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-logr/logr"
+	mv1 "github.com/llmariner/model-manager/api/v1"
+	"github.com/llmariner/rbac-manager/pkg/auth"
+	"google.golang.org/grpc"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const modelListInterval = 10 * time.Second
+
+// ModelManager is an interface for managing models.
+type ModelManager interface {
+	PullModel(ctx context.Context, modelID string) error
+	DeleteModel(ctx context.Context, modelID string) error
+}
+
+type modelLister interface {
+	ListModels(ctx context.Context, in *mv1.ListModelsRequest, opts ...grpc.CallOption) (*mv1.ListModelsResponse, error)
+}
+
+// NewModelActivator creates a new ModelActivator.
+func NewModelActivator(mmanager ModelManager, modelLister modelLister) *ModelActivator {
+	return &ModelActivator{
+		mmanager:    mmanager,
+		modelLister: modelLister,
+	}
+}
+
+// ModelActivator preloads models.
+type ModelActivator struct {
+	mmanager ModelManager
+
+	modelLister modelLister
+
+	logger logr.Logger
+}
+
+// SetupWithManager sets up the multi-autoscaler with the Manager.
+func (a *ModelActivator) SetupWithManager(mgr ctrl.Manager) error {
+	a.logger = mgr.GetLogger().WithName("preloader")
+	return mgr.Add(a)
+}
+
+// Start starts the multi-autoscaler.
+func (a *ModelActivator) Start(ctx context.Context) error {
+	ctx = ctrl.LoggerInto(ctx, a.logger)
+
+	a.logger.Info("Starting model activator")
+
+	for {
+		if err := a.reconcileModelActivation(ctx); err != nil {
+			return fmt.Errorf("reconcile model activation: %s", err)
+		}
+		select {
+		case <-time.After(modelListInterval):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (a *ModelActivator) reconcileModelActivation(ctx context.Context) error {
+	ctx = auth.AppendWorkerAuthorization(ctx)
+
+	resp, err := a.modelLister.ListModels(ctx, &mv1.ListModelsRequest{})
+	if err != nil {
+		return fmt.Errorf("list models: %s", err)
+	}
+	for _, model := range resp.Data {
+		switch model.ActivationStatus {
+		case mv1.ActivationStatus_ACTIVATION_STATUS_UNSPECIFIED:
+			// Do nothing for backward compatibility.
+		case mv1.ActivationStatus_ACTIVATION_STATUS_ACTIVE:
+			if err := a.mmanager.PullModel(ctx, model.Id); err != nil {
+				return fmt.Errorf("pull model %s: %s", model.Id, err)
+			}
+		case mv1.ActivationStatus_ACTIVATION_STATUS_INACTIVE:
+			if err := a.mmanager.DeleteModel(ctx, model.Id); err != nil {
+				return fmt.Errorf("delete model %s: %s", model.Id, err)
+			}
+		default:
+			return fmt.Errorf("unknown activation state: %s", model.ActivationStatus)
+		}
+	}
+
+	a.logger.Info("Model activation reconciled", "modelCount", len(resp.Data))
+	return nil
+}
+
+// NeedLeaderElection implements LeaderElectionRunnable and always returns true.
+func (a *ModelActivator) NeedLeaderElection() bool {
+	return true
+}
---
+++ engine/internal/runtime/model_activator_test.go
@@ -0,0 +1,63 @@
+package runtime
+
+import (
+	"context"
+	"testing"
+
+	mv1 "github.com/llmariner/model-manager/api/v1"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+)
+
+func TestReconcileModelActivation(t *testing.T) {
+	mmanager := &fakeModelManager{}
+
+	fakeModelLister := &fakeModelLister{
+		resp: &mv1.ListModelsResponse{
+			Data: []*mv1.Model{
+				{
+					Id:               "model1",
+					ActivationStatus: mv1.ActivationStatus_ACTIVATION_STATUS_ACTIVE,
+				},
+				{
+					Id:               "model2",
+					ActivationStatus: mv1.ActivationStatus_ACTIVATION_STATUS_INACTIVE,
+				},
+				{
+					Id:               "model3",
+					ActivationStatus: mv1.ActivationStatus_ACTIVATION_STATUS_UNSPECIFIED,
+				},
+			},
+		},
+	}
+
+	a := NewModelActivator(mmanager, fakeModelLister)
+	err := a.reconcileModelActivation(context.Background())
+	assert.NoError(t, err)
+
+	assert.ElementsMatch(t, []string{"model1"}, mmanager.pulled)
+	assert.ElementsMatch(t, []string{"model2"}, mmanager.deleted)
+}
+
+type fakeModelManager struct {
+	pulled  []string
+	deleted []string
+}
+
+func (f *fakeModelManager) PullModel(ctx context.Context, modelID string) error {
+	f.pulled = append(f.pulled, modelID)
+	return nil
+}
+
+func (f *fakeModelManager) DeleteModel(ctx context.Context, modelID string) error {
+	f.deleted = append(f.deleted, modelID)
+	return nil
+}
+
+type fakeModelLister struct {
+	resp *mv1.ListModelsResponse
+}
+
+func (f *fakeModelLister) ListModels(ctx context.Context, req *mv1.ListModelsRequest, opts ...grpc.CallOption) (*mv1.ListModelsResponse, error) {
+	return f.resp, nil
+}

e59ad79dadad3cd84e5df60789e7abda05d0ccb8
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-05-22 16:23:07 -0700

    chore: do not log an error if successful (#853)

--- engine/internal/processor/processor.go
+++ engine/internal/processor/processor.go
@@ -243,7 +243,9 @@ func (p *P) runInternal(ctx context.Context, engineID string) error {
 	// Wait for the first error from either sendEngineStatusPeriodically or processTasks.
 	// Then cancel the context to stop both goroutines.
 	err = <-errCh
-	p.logger.Error(err, "Processor error")
+	if err != nil {
+		p.logger.Error(err, "Processor error")
+	}
 	cancel()
 	<-errCh
 	return err

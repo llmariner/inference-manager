d25caa0c46b445e14f6c449d039dfaf30db9904e
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-08-12 16:06:27 -0700

    feat: add a proto def for CreateModelResponseRequest

---
+++ ts/api/v1/inference_server_model_response.pb.ts
@@ -0,0 +1,93 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+export type CreateModelResponseRequestInputContent = {
+  type?: string
+  text?: string
+  detail?: string
+  image_url?: string
+  file_id?: string
+  file_data?: string
+  file_url?: string
+  filename?: string
+}
+
+export type CreateModelResponseRequestInput = {
+  type?: string
+  content?: CreateModelResponseRequestInputContent[]
+  role?: string
+  status?: string
+  id?: string
+}
+
+export type CreateModelResponseRequestPrompt = {
+  id?: string
+  version?: string
+}
+
+export type CreateModelResponseRequestReasoning = {
+  effort?: string
+  generate_summary?: string
+  summary?: string
+}
+
+export type CreateModelResponseRequestStreamOptions = {
+  include_obfuscation?: boolean
+}
+
+export type CreateModelResponseRequestTextFormat = {
+  type?: string
+  name?: string
+  encoded_schema?: string
+  description?: string
+  strict?: boolean
+}
+
+export type CreateModelResponseRequestText = {
+  format?: CreateModelResponseRequestTextFormat
+  verbosity?: string
+}
+
+export type CreateModelResponseRequestToolChoice = {
+  type?: string
+  name?: string
+  mode?: string
+  encoded_tools?: string
+  server_label?: string
+}
+
+export type CreateModelResponseRequest = {
+  background?: boolean
+  include?: string[]
+  input?: CreateModelResponseRequestInput[]
+  instructions?: string
+  max_output_tokens?: number
+  max_tool_calls?: number
+  metadata?: {[key: string]: string}
+  model?: string
+  parallel_tool_calls?: boolean
+  previous_response_id?: string
+  prompt?: CreateModelResponseRequestPrompt
+  prompt_cache_key?: string
+  reasoning?: CreateModelResponseRequestReasoning
+  safety_identifier?: string
+  service_tier?: string
+  store?: boolean
+  stream?: boolean
+  stream_options?: CreateModelResponseRequestStreamOptions
+  temperature?: number
+  is_temperature_set?: boolean
+  text?: CreateModelResponseRequestText
+  tool_choice?: string
+  tool_choice_object?: CreateModelResponseRequestToolChoice
+  top_logprobs?: number
+  top_p?: number
+  is_top_p_set?: boolean
+  truncation?: boolean
+  user?: string
+}
+
+export type ModelResponse = {
+}

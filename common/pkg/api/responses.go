f8a0836f922321e691043c5a958859bb4657f1dc
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-08-13 23:03:25 -0700

    fix: add the 'encoded_tools' field to CreateModelResponseRequest

--- common/pkg/api/responses.go
+++ common/pkg/api/responses.go
@@ -8,6 +8,7 @@ import (
 // ConvertCreateModelResponseRequestToProto converts the request to the protobuf format.
 func ConvertCreateModelResponseRequestToProto(body []byte) ([]byte, error) {
 	fs := []convertF{
+		convertTools,
 		convertToolChoiceTools,
 		convertToolChoice,
 		convertTemperature,
@@ -27,6 +28,7 @@ func ConvertCreateModelResponseRequestToOpenAI(body []byte) ([]byte, error) {
 		convertEncodedTemperature,
 		convertToolChoiceObject,
 		convertEncodedToolChoiceTools,
+		convertEncodedTools,
 	}
 	return applyConvertFuncs(body, fs)
 }
@@ -88,6 +90,48 @@ func convertEncodedToolChoiceTools(r map[string]interface{}) error {
 
 	t["tools"] = pp
 	delete(t, "encoded_tools")
+
+	return nil
+}
+
+func convertTools(r map[string]interface{}) error {
+	tools, ok := r["tools"]
+	if !ok {
+		return nil
+	}
+	p, ok := tools.([]interface{})
+	if !ok {
+		return nil
+	}
+
+	pp, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+
+	r["encoded_tools"] = base64.URLEncoding.EncodeToString(pp)
+	delete(r, "tools")
+	return nil
+}
+
+func convertEncodedTools(r map[string]interface{}) error {
+	p, ok := r["encoded_tools"]
+	if !ok {
+		return nil
+	}
+
+	b, err := base64.URLEncoding.DecodeString(p.(string))
+	if err != nil {
+		return err
+	}
+
+	pp := []interface{}{}
+	if err := json.Unmarshal(b, &pp); err != nil {
+		return err
+	}
+
+	r["tools"] = pp
+	delete(r, "encoded_tools")
 
 	return nil
 }

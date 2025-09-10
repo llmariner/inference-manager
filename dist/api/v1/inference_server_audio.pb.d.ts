2c6b505f478bb66a302f66e04913f66128a0a260
Takeshi Yoshino <81723316+takeshi-cloudnatix@users.noreply.github.com>
2025-09-10 23:28:29 +0000

    chore(dist): build `dist` files

---
+++ dist/api/v1/inference_server_audio.pb.d.ts
@@ -0,0 +1,26 @@
+export type CreateAudioTranscriptionRequest = {
+    file?: Uint8Array;
+    filename?: string;
+    model?: string;
+    language?: string;
+    prompt?: string;
+    response_format?: string;
+    stream?: boolean;
+    temperature?: number;
+};
+export type TranscriptionUsageInputTokenDetails = {
+    audio_tokens?: number;
+    text_tokens?: number;
+};
+export type TranscriptionUsage = {
+    type?: string;
+    input_tokens?: number;
+    output_tokens?: number;
+    total_tokens?: number;
+    input_token_details?: TranscriptionUsageInputTokenDetails;
+    seconds?: number;
+};
+export type Transcription = {
+    text?: string;
+    usage?: TranscriptionUsage;
+};

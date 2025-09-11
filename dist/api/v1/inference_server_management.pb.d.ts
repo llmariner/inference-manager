6ac56f922f52e46b26df6ad8cd64ccd984a42f05
Takeshi Yoshino <81723316+takeshi-cloudnatix@users.noreply.github.com>
2025-09-11 09:28:19 +0900

    chore(dist): build `dist` files (#1038)
    ```
    buf --version
    1.52.1
    ```
    
    ```
    tsc --version
    Version 5.6.3
    ```
    
    removed the contents under the `ts` directory except for `index.ts`
    (manually created file. See
    https://buf.build/blog/grpc-microservice-with-connect).
    
    ```
    make generate                          
    buf dep update
    buf generate
    buf generate --template buf.gen.ts.yaml --include-imports --include-wkt
    rm -fr ./dist
    tsc
    ```

---
+++ dist/api/v1/inference_server_management.pb.d.ts
@@ -0,0 +1,41 @@
+import * as fm from "../../fetch.pb";
+export type EngineStatusModelStatusDetails = {
+    num_ready_pods?: number;
+    num_total_pods?: number;
+    status_message?: string;
+};
+export type EngineStatusModel = {
+    id?: string;
+    is_ready?: boolean;
+    in_progress_task_count?: number;
+    gpu_allocated?: number;
+    is_dynamically_loaded_lora?: boolean;
+    status_details?: EngineStatusModelStatusDetails;
+};
+export type EngineStatus = {
+    engine_id?: string;
+    ready?: boolean;
+    models?: EngineStatusModel[];
+    cluster_id?: string;
+};
+export type InferenceStatus = {
+    cluster_statuses?: ClusterStatus[];
+    task_status?: TaskStatus;
+};
+export type TaskStatus = {
+    in_progress_task_counts?: {
+        [key: string]: number;
+    };
+};
+export type ClusterStatus = {
+    id?: string;
+    name?: string;
+    engine_statuses?: EngineStatus[];
+    model_count?: number;
+    in_progress_task_count?: number;
+    gpu_allocated?: number;
+};
+export type GetInferenceStatusRequest = {};
+export declare class InferenceService {
+    static GetInferenceStatus(req: GetInferenceStatusRequest, initReq?: fm.InitReq): Promise<InferenceStatus>;
+}
